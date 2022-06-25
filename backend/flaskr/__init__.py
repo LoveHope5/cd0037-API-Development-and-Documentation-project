f3ca8ef4865b427fadab632735ec1150bd53ad07
LoveHope5 <dimitrimehelo@gmail.com>
2022-06-25 23:39:15 +0100

    update

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,20 +12,36 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    CORS(app, resources={r"*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route("/categories")
+    def retrieve_categories():
+
+        return jsonify({
+            "success":True
+        })
 
 
     """
@@ -40,6 +56,13 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route("/questions")
+    def retrieve_questions():
+
+        return jsonify({
+            "success":True
+        })
+
 
     """
     @TODO:
@@ -48,6 +71,13 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question(question_id):
+
+        return jsonify({
+            "success":True
+        })
+
 
     """
     @TODO:
@@ -60,6 +90,7 @@ def create_app(test_config=None):
     of the questions list in the "List" tab.
     """
 
+
     """
     @TODO:
     Create a POST endpoint to get questions based on a search term.
@@ -68,17 +99,57 @@ def create_app(test_config=None):
 
     TEST: Search by any phrase. The questions list will update to include
     only question that include that string within their question.
-    Try using the word "title" to start.
+    Try using the word "title" to start. 
     """
+    @app.route("/questions", methods=["POST"])
+    def create_question():
+        body = request.get_json()
+        search = body.get("search", None)
+        try:
+            if search:
+                
+
+                return jsonify(
+                    {
+                        "success": True,
+                      
+                       
+                    }
+                )
+
+            else:
+                
+
+                return jsonify(
+                    {
+                        "success": True,
+                         
+                       
+                    }
+                )
+
+        except:
+            abort(422)
+
+         
+
+ 
 
     """
     @TODO:
     Create a GET endpoint to get questions based on category.
 
     TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
+    categories in the left column will cause only questions of that0
     category to be shown.
     """
+    @app.route("/categories/<int:id>/questions")
+    def view_question_categories(question_id):
+
+        return jsonify({
+            "success":True
+        })
+
 
     """
     @TODO:
@@ -91,6 +162,12 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route("/quizz", methods=["POST"])
+    def _question(question_id):
+
+        return jsonify({
+            "success":True
+        })
 
     """
     @TODO:
